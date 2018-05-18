eb59d077a79eb1d3ce531e69a3a42126347b873c
Zhihui Zhang <zhhzhang@CNzhhzhangP.local>
2018-05-18 13:48:58 +0800

    FizzBuzzWhizz game

--- src/main/java/com/thoughtworks/FizzBuzzGame.java
+++ src/main/java/com/thoughtworks/FizzBuzzGame.java
@@ -12,8 +12,202 @@ public class FizzBuzzGame {
     }
 
     public List<String> getResults() {
+
         return results;
     }
+
+    public ArrayList<String> numberIs3(int n) {
+        String temp = String.valueOf(n);
+        ArrayList<String> digits = new ArrayList<>();
+        System.out.println(temp);
+
+        String result = "";
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 != 0 && i % 7 != 0 || String.valueOf(i).contains("3")) {
+                result = "Fizz";
+                digits.add(result);
+            } else {
+                result = String.valueOf(i);
+                digits.add(result);
+            }
+        }
+        return digits;
+    }
+
+    public ArrayList<String> numberIs5(int n) {
+        String temp = String.valueOf(n);
+        ArrayList<String> digits = new ArrayList<>();
+        System.out.println(temp);
+
+        String result = "";
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0) {
+                result = "Fizz";
+                digits.add(result);
+            } else if (i % 3 != 0 && i % 5 == 0 && i % 7 != 0 || String.valueOf(i).contains("5")) {
+                result = "Buzz";
+                digits.add(result);
+            } else {
+                result = String.valueOf(i);
+                digits.add(result);
+            }
+        }
+        return digits;
+    }
+
+    public ArrayList<String> numberIs7(int n) {
+        String temp = String.valueOf(n);
+        ArrayList<String> digits = new ArrayList<>();
+        System.out.println(temp);
+
+        String result = "";
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 || String.valueOf(i).contains("3")) {
+                result = "Fizz";
+                digits.add(result);
+            } else if (i % 5 == 0 || String.valueOf(i).contains("5")) {
+                result = "Buzz";
+                digits.add(result);
+            } else if (i % 3 != 0 && i % 5 != 0 && i % 7 == 0 || String.valueOf(i).contains("7")) {
+                result = "Whizz";
+                digits.add(result);
+            } else {
+                result = String.valueOf(i);
+                digits.add(result);
+            }
+        }
+        return digits;
+    }
+
+    public ArrayList<String> numberIs3_and_5(int n) {
+        String temp = String.valueOf(n);
+        ArrayList<String> digits = new ArrayList<>();
+        System.out.println(temp);
+
+        String result = "";
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 != 0 || String.valueOf(i).contains("3")) {
+                result = "Fizz";
+                digits.add(result);
+            } else if (i % 3 == 0 && i % 5 == 0) {
+                result = "FizzBuzz";
+                digits.add(result);
+            } else if (i % 5 == 0) {
+                result = "Buzz";
+                digits.add(result);
+            } else if (i % 3 != 0 && i % 5 != 0 && i % 7 == 0) {
+                result = "Whizz";
+                digits.add(result);
+            } else {
+                result = String.valueOf(i);
+                digits.add(result);
+            }
+        }
+        return digits;
+    }
+
+    public ArrayList<String> numberIs3_and_7(int n) {
+        String temp = String.valueOf(n);
+        ArrayList<String> digits = new ArrayList<>();
+        System.out.println(temp);
+
+        String result = "";
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                result = "FizzBuzz";
+                digits.add(result);
+            } else if (i % 3 == 0 && i % 7 == 0) {
+                result = "FizzWhizz";
+                digits.add(result);
+            } else if (i % 3 == 0 && i % 5 != 0 || String.valueOf(i).contains("3")) {
+                result = "Fizz";
+                digits.add(result);
+            } else if (i % 5 == 0) {
+                result = "Buzz";
+                digits.add(result);
+            } else if (i % 3 != 0 && i % 5 != 0 && i % 7 == 0) {
+                result = "Whizz";
+                digits.add(result);
+            } else {
+                result = String.valueOf(i);
+                digits.add(result);
+            }
+        }
+        return digits;
+    }
+
+    public ArrayList<String> numberIs5_and_7(int n) {
+        String temp = String.valueOf(n);
+        ArrayList<String> digits = new ArrayList<>();
+        System.out.println(temp);
+
+        String result = "";
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                result = "FizzBuzz";
+                digits.add(result);
+            } else if (i % 3 == 0 && i % 7 == 0) {
+                result = "FizzWhizz";
+                digits.add(result);
+            } else if (i % 3 == 0 && i % 5 != 0 || String.valueOf(i).contains("3")) {
+                result = "Fizz";
+                digits.add(result);
+            } else if (i % 5 == 0 && i % 7 == 0) {                                             //execute sequence
+                result = "BuzzWhizz";
+                digits.add(result);
+            } else if (i % 5 == 0) {
+                result = "Buzz";
+                digits.add(result);
+            } else if (i % 3 != 0 && i % 5 != 0 && i % 7 == 0) {
+                result = "Whizz";
+                digits.add(result);
+            } else {
+                result = String.valueOf(i);
+                digits.add(result);
+            }
+        }
+        return digits;
+    }
+
+    public ArrayList<String> numberIs3_and_5_and_7(int n) {
+        String temp = String.valueOf(n);
+        ArrayList<String> digits = new ArrayList<>();
+        System.out.println(temp);
+
+        String result = "";
+        for (int i = 1; i <= n; i++) {
+            if(i % 3 ==0 && i % 5 == 0 && i % 7 == 0) {
+                result = "FizzBuzzWhizz";
+                digits.add(result);
+            }else if(String.valueOf(i).contains("3")){
+                result = "Fizz";
+                digits.add(result);
+            } else if (i % 3 == 0 && i % 5 == 0) {
+                result = "FizzBuzz";
+                digits.add(result);
+            } else if (i % 3 == 0 && i % 7 == 0) {
+                result = "FizzWhizz";
+                digits.add(result);
+            } else if (i % 3 == 0 && i % 5 != 0 || String.valueOf(i).contains("3")) {
+                result = "Fizz";
+                digits.add(result);
+            } else if (i % 5 == 0 && i % 7 == 0) {                                             //execute sequence
+                result = "BuzzWhizz";
+                digits.add(result);
+            } else if (i % 5 == 0) {
+                result = "Buzz";
+                digits.add(result);
+            } else if (i % 7 == 0) {
+                result = "Whizz";
+                digits.add(result);
+            } else {
+                result = String.valueOf(i);
+                digits.add(result);
+            }
+        }
+        return digits;
+    }
 }
+
 
 
--- src/test/java/com/thoughtworks/FizzBuzzGameTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzGameTest.java
@@ -2,9 +2,162 @@ package com.thoughtworks;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
 public class FizzBuzzGameTest {
+
+    FizzBuzzGame fizzBuzzGame = new FizzBuzzGame();
+
     @Test
     public void should_return_Fizz_when_have_3() {
+        ArrayList<String> fizzList = new ArrayList<>();
+        ArrayList<String> result = new ArrayList<>();
+        fizzList.add("1");
+        fizzList.add("2");
+        fizzList.add("Fizz");
+
+        result = fizzBuzzGame.numberIs3(3);
+        assertThat(fizzList).isEqualTo(result);
+    }
+
+    @Test
+    public void numberIs5() {
+        ArrayList<String> fizzList = new ArrayList<>();
+        ArrayList<String> result = new ArrayList<>();
+        fizzList.add("1");
+        fizzList.add("2");
+        fizzList.add("Fizz");
+        fizzList.add("4");
+        fizzList.add("Buzz");
+
+        result = fizzBuzzGame.numberIs5(5);
+        assertThat(fizzList).isEqualTo(result);
+    }
+
+    @Test
+    public void numberIs7() {
+        ArrayList<String> fizzList = new ArrayList<>();
+        ArrayList<String> result = new ArrayList<>();
+        fizzList.add("1");
+        fizzList.add("2");
+        fizzList.add("Fizz");
+        fizzList.add("4");
+        fizzList.add("Buzz");
+        fizzList.add("Fizz");
+        fizzList.add("Whizz");
+
+        result = fizzBuzzGame.numberIs7(7);
+        assertThat(fizzList).isEqualTo(result);
+    }
+
+    @Test
+    public void numberIs3_and_5() {
+        ArrayList<String> fizzList = new ArrayList<>();
+        ArrayList<String> result = new ArrayList<>();
+        fizzList.add("1");
+        fizzList.add("2");
+        fizzList.add("Fizz");
+        fizzList.add("4");
+        fizzList.add("Buzz");
+        fizzList.add("Fizz");
+        fizzList.add("Whizz");
+        fizzList.add("8");
+        fizzList.add("Fizz");
+        fizzList.add("Buzz");
+        fizzList.add("11");
+        fizzList.add("Fizz");
+        fizzList.add("Fizz");
+        fizzList.add("Whizz");
+        fizzList.add("FizzBuzz");
+        result = fizzBuzzGame.numberIs3_and_5(15);
+        assertThat(fizzList).isEqualTo(result);
+    }
+
+
+    @Test
+    public void numberIs3_and_7() {
+        ArrayList<String> fizzList = new ArrayList<>();
+        ArrayList<String> result = new ArrayList<>();
+        fizzList.add("1");
+        fizzList.add("2");
+        fizzList.add("Fizz");
+        fizzList.add("4");
+        fizzList.add("Buzz");
+        fizzList.add("Fizz");
+        fizzList.add("Whizz");
+        fizzList.add("8");
+        fizzList.add("Fizz");
+        fizzList.add("Buzz");
+        fizzList.add("11");
+        fizzList.add("Fizz");
+        fizzList.add("Fizz");
+        fizzList.add("Whizz");
+        fizzList.add("FizzBuzz");
+        fizzList.add("16");
+        fizzList.add("17");
+        fizzList.add("Fizz");
+        fizzList.add("19");
+        fizzList.add("Buzz");
+        fizzList.add("FizzWhizz");
+
+        result = fizzBuzzGame.numberIs3_and_7(21);
+        assertThat(fizzList).isEqualTo(result);
+    }
+
+    @Test
+    public void numberIs5_and_7() {
+        ArrayList<String> fizzList = new ArrayList<>();
+        ArrayList<String> result = new ArrayList<>();
+        fizzList.add("1");
+        fizzList.add("2");
+        fizzList.add("Fizz");
+        fizzList.add("4");
+        fizzList.add("Buzz");
+        fizzList.add("Fizz");
+        fizzList.add("Whizz");
+        fizzList.add("8");
+        fizzList.add("Fizz");
+        fizzList.add("Buzz");
+        fizzList.add("11");
+        fizzList.add("Fizz");
+        fizzList.add("Fizz");
+        fizzList.add("Whizz");
+        fizzList.add("FizzBuzz");
+        fizzList.add("16");
+        fizzList.add("17");
+        fizzList.add("Fizz");
+        fizzList.add("19");
+        fizzList.add("Buzz");
+        fizzList.add("FizzWhizz");
+        fizzList.add("22");
+        fizzList.add("Fizz");
+        fizzList.add("Fizz");
+
+        result = fizzBuzzGame.numberIs5_and_7(24);
+        assertThat(fizzList).isEqualTo(result);
+    }
+
+    @Test
+    public void numberIs3_and_5_and_7() {
+        List<String> expectedResults = Arrays.asList(
+                "1", "2", "Fizz", "4", "Buzz", "Fizz", "Whizz", "8", "Fizz", "Buzz",
+                "11", "Fizz", "Fizz", "Whizz", "FizzBuzz", "16", "17", "Fizz", "19", "Buzz",
+                "FizzWhizz", "22", "Fizz", "Fizz", "Buzz", "26", "Fizz", "Whizz", "29", "Fizz",
+                "Fizz", "Fizz", "Fizz", "Fizz", "Fizz", "Fizz", "Fizz", "Fizz", "Fizz", "Buzz",
+                "41", "FizzWhizz", "Fizz", "44", "FizzBuzz", "46", "47", "Fizz", "Whizz", "Buzz",
+                "Fizz", "52", "Fizz", "Fizz", "Buzz", "Whizz", "Fizz", "58", "59", "FizzBuzz",
+                "61", "62", "Fizz", "64", "Buzz", "Fizz", "67", "68", "Fizz", "BuzzWhizz",
+                "71", "Fizz", "Fizz", "74", "FizzBuzz", "76", "Whizz", "Fizz", "79", "Buzz",
+                "Fizz", "82", "Fizz", "FizzWhizz", "Buzz", "86", "Fizz", "88", "89", "FizzBuzz",
+                "Whizz", "92", "Fizz", "94", "Buzz", "Fizz", "97", "Whizz", "Fizz", "Buzz");
+        ArrayList<String> result = new ArrayList<>();
+        result = fizzBuzzGame.numberIs3_and_5_and_7(100);
+        assertThat(expectedResults).isEqualTo(result);
 
     }
 }
+
